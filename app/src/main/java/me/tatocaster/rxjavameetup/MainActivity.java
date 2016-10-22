468322d0d933b31cb52327c6508dafc9d99b2fb0
Merab Kutalia <kutaliatato@gmail.com>
2016-10-23 03:13:11 +0400

    simple operators and CompositeSubscription

---
+++ app/src/main/java/me/tatocaster/rxjavameetup/MainActivity.java
@@ -0,0 +1,111 @@
+package me.tatocaster.rxjavameetup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func0;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private Subscription mSubscriptionUsingCreate;
+    private Observer mObserver;
+    private CompositeSubscription mCompositeSubscription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCompositeSubscription = new CompositeSubscription();
+
+        mObserver = new Observer<Object>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted() called");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError() called with: e = [" + e + "]");
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Log.d(TAG, "onNext() called with: o = [" + o + "]");
+            }
+        };
+
+        mSubscriptionUsingCreate = usingCreate().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(mObserver);
+
+        Subscription subscriptionUsingJust = usingJust().subscribe(new Observer<Object>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted() called");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError() called with: e = [" + e + "]");
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Log.d(TAG, "onNext() called with: o = [" + o + "]");
+            }
+        });
+        mCompositeSubscription.add(subscriptionUsingJust);
+        usingJust();
+        usingCallable();
+
+    }
+
+    private Observable<Object> usingCreate() {
+        return Observable.create(new Observable.OnSubscribe<Object>() {
+            @Override
+            public void call(Subscriber<? super Object> subscriber) {
+                if (!subscriber.isUnsubscribed()) subscriber.onNext("Hello");
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    private Observable<String> usingJust() {
+        return Observable.just("Hello");
+    }
+
+    private void usingCallable() {
+        Observable.fromCallable(new Func0<Object>() {
+            @Override
+            public Object call() {
+                return "ABC";
+            }
+        })
+                .subscribe(mObserver);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+
+        /*
+        unsubscribe single sub
+         */
+        if (mSubscriptionUsingCreate != null) mSubscriptionUsingCreate.unsubscribe();
+
+        /*
+        composite subscriptions list
+         */
+        mCompositeSubscription.unsubscribe();
+        super.onDestroy();
+    }
+}
